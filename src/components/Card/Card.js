d1551b10ab8c08b9ce88ec2177f52de761e493af
Antoine Cadoret <antoinecadoret97@gmail.com>
2022-05-24 19:34:53 +0200

    [Card] styling and structuring

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,10 @@
-const Card = (props,item) =>{
-    return(<div className="card">
-        <h2>{item.breed}</h2>
-    </div>);
-   
-}
+import "./Card.css";
+const Card = ({ catBreed }, props) => {
+  return (
+    <div className="card rounded-md m-4 p-1 md:container md:mx-auto">
+      <h2 className="text-regal-blue">{catBreed.breed}</h2>
+    </div>
+  );
+};
 
 export default Card;
