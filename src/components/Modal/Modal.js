d5e00a10636328c597633f3cd51fbc539cc5e98a
Antoine Cadoret <antoinecadoret97@gmail.com>
2022-05-26 19:59:30 +0200

    [Modal] styling

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -7,14 +7,18 @@ const Modal = ({ fact, modalCallback, visible }, props) => {
   return (
     <div className={visible ? "block" : "hidden"}>
       <div className="bg-black/50 w-full h-full fixed top-0 right-0 left-0 z-300" onClick={() => unShow()}>
-        <div className="modal rounded-md bg-white fixed top-0 right-0 left-0 z-500 w-3/4 m-auto md:inset-0 h-modal md:h-3/4">
-          <h4 className="p-4 bg-slate-800 text-white font-bold">
+        <div className="modal rounded-md bg-gray-200 fixed top-0 right-0 left-0 z-500 w-1/3 m-auto md:inset-0 h-modal md:h-1/3 flex flex-col">
+          <div className="flex flex-row justify-between bg-slate-800 text-white font-bold">
+          <h4 className="p-4">
             A random fact on Cats
           </h4>
-          <p className="p-8">{fact}</p>
-          <button className="" onClick={() => unShow()}>
-            close
+          <button className="px-8" onClick={() => unShow()}>
+            X
           </button>
+          </div>
+          
+          <p className="my-4 p-8 self-center">{fact}</p>
+          
         </div>
       </div>
     </div>
