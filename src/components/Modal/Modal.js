a1b2598cfc79be952ead39a9b8ba9415b1f92658
Antoine Cadoret <antoinecadoret97@gmail.com>
2022-05-26 17:25:42 +0200

    [Modal] giving opacity to the background

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 
 const Modal = ({ fact, modalCallback, visible }, props) => {
-
   const unShow = () => {
     modalCallback();
   };
   return (
     <div className={visible ? "block" : "hidden"}>
-      <div className="modal rounded-md bg-white fixed top-0 right-0 left-0 z-50 w-3/4 m-auto md:inset-0 h-modal md:h-3/4">
-        <h4 className="p-4 bg-slate-800 text-white font-bold">
-          A random fact on Cats
-        </h4>
-        <p className="p-8">{fact}</p>
-        <button className="" onClick={() => unShow()}>close</button>
+      <div className="bg-black/50 w-full h-full fixed top-0 right-0 left-0 z-300" onClick={() => unShow()}>
+        <div className="modal rounded-md bg-white fixed top-0 right-0 left-0 z-500 w-3/4 m-auto md:inset-0 h-modal md:h-3/4">
+          <h4 className="p-4 bg-slate-800 text-white font-bold">
+            A random fact on Cats
+          </h4>
+          <p className="p-8">{fact}</p>
+          <button className="" onClick={() => unShow()}>
+            close
+          </button>
+        </div>
       </div>
     </div>
   );
